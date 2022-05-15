f599420085aae2ee09b84640c61a2e1959c7df7d
Mikiyas721 <Mikiyas721@gmail.com>
2022-05-15 10:54:06 -0600

    Added checkPhoneNumber and corrected model relations

--- server/models/user.js
+++ server/models/user.js
@@ -1,162 +1,207 @@
 'use strict';
 
 module.exports = function(User) {
-  /**Add User method*/
-  User.remoteMethod('addUser', {
+  /**Validation*/
+  User.validatesUniquenessOf('phoneNumber', {message: 'Phone Number has to be unique.'});
+  /**-------------------------------- End ------------------------------------*/
+
+  /**---------------------------- SignIn user --------------------------------*/
+  User.remoteMethod('signIn', {
     accepts: {
-      arg: 'user',
+      arg: 'credentials',
       type: {
+        phoneNumber: 'string',
+        password: 'string',
+      },
+      required: true,
+      http: {source: 'body'},
+    },
+    returns: {
+      arg: 'app user',
+      type: {
+        id: 'string',
         imageUrl: 'string',
         firstName: 'string',
         lastName: 'string',
         phoneNumber: 'string',
-        country: 'string',
-        city: 'string',
-        email: 'string',
-        website: 'string',
-        password: 'string',
         companyName: 'string',
         accountType: 'string',
         categories: [
           'string',
         ],
-        address: 'string',
-        completeAddress: 'string',
-        gstNo: 'string',
-        tanNo: 'string',
-        gstDocumentUrl: 'string',
-        panNo: 'string',
-        panCardUrl: 'string',
-        accountName: 'string',
-        accountNumber: 'number',
-        iFSCCode: 'string',
-        bankName: 'string',
-        bankBranch: 'string',
-        bankState: 'string',
-        bankCity: 'string',
-        addressProofUrl: 'string',
-        cancelledChequeUrl: 'string',
+        businessDetailsId: 'string',
+        bankDetailsId: 'string',
+      },
+      root: true,
+    },
+    description: 'Returns app user if exists',
+  });
+
+  User.signIn = async function(credentials) {
+    const fetchedUserResponse = await User.findOne({
+      where: {phoneNumber: {eq: credentials.phoneNumber}},
+      include: ["bankDetail","businessDetail"]
+    });
+    if (fetchedUserResponse != undefined) {
+      const fetchedUserJson = fetchedUserResponse.toJSON()
+      if (fetchedUserJson.password === fetchedUserJson.password) {
+        return {
+          id: fetchedUserJson.id,
+          imageUrl: fetchedUserJson.imageUrl,
+          firstName: fetchedUserJson.firstName,
+          lastName: fetchedUserJson.lastName,
+          phoneNumber: fetchedUserJson.phoneNumber,
+          companyName: fetchedUserJson.businessDetail.companyName,
+          accountType: fetchedUserJson.businessDetail.accountType,
+          categories: fetchedUserJson.businessDetail.categories,
+          businessDetailId: fetchedUserJson.businessDetail.id,
+          bankDetailId: fetchedUserJson.bankDetail.id,
+        };
+      } else {
+        throw Error('Incorrect password');
+      }
+    } else {
+      throw Error('User not found');
+    }
+  };
+  /**-------------------------------- End ------------------------------------*/
+
+  /**---------------------------- SignUp user --------------------------------*/
+  User.remoteMethod('addUser', {
+    accepts: {
+      arg: 'user',
+      type: {
+        phoneNumber: 'string',
+        accountType: 'string',
+        categories: [
+          'string',
+        ],
+        firstName: 'string',
+        lastName: 'string',
+        companyName: 'string',
+        password: 'string',
       },
       required: true,
       http: {source: 'body'},
     },
     returns: {
       arg: 'user',
-      type: 'object',
+      type: {
+        id: 'string',
+        imageUrl: 'string',
+        firstName: 'string',
+        lastName: 'string',
+        phoneNumber: 'string',
+        companyName: 'string',
+        accountType: 'string',
+        categories: [
+          'string',
+        ],
+        businessDetailsId: 'string',
+        bankDetailsId: 'string',
+      },
       root: true,
     },
-    description: 'Adds user a corresponding bank and business details',
+    description: 'Adds new user with corresponding bank and business details',
   });
 
   User.addUser = async function(user) {
     if (
       !(user.firstName === undefined ||
         user.phoneNumber === undefined ||
-        user.password === undefined)
+        user.password === undefined ||
+        user.companyName === undefined ||
+        user.accountType === undefined ||
+        user.categories.length === 0
+      )
     ) {
-      const businessDetails = await User.app.models.businessDetail.create(
-        {
-          companyName: user.companyName,
-          accountType: user.accountType,
-          categories: user.categories,
-          address: user.address,
-          completeAddress: user.completeAddress,
-          gstNo: user.gstNo,
-          tanNo: user.tanNo,
-          gstDocumentUrl: user.gstDocumentUrl,
-          panNo: user.panNo,
-          panCardUrl: user.panCardUrl,
-        },
-      );
-      const bankDetails = await User.app.models.bankDetail.create(
-        {
-          accountName: user.accountName,
-          accountNumber: user.accountNumber,
-          iFSCCode: user.iFSCCode,
-          bankName: user.bankName,
-          bankBranch: user.bankBranch,
-          bankState: user.bankState,
-          bankCity: user.bankCity,
-          addressProofUrl: user.addressProofUrl,
-          cancelledChequeUrl: user.cancelledChequeUrl,
-        },
-      );
+      const registeredWithPhoneNumber = await User.findOne({where: {phoneNumber: {eq: user.phoneNumber}}});
+      if (registeredWithPhoneNumber == undefined) {
+        const newUser = await User.create(
+          {
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            phoneNumber: user.phoneNumber,
+            country: user.country,
+            city: user.city,
+            email: user.email,
+            website: user.website,
+            password: user.password
+          },
+        );
 
-      return await User.create(
-        {
-          imageUrl: user.imageUrl,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          phoneNumber: user.phoneNumber,
-          country: user.country,
-          city: user.city,
-          email: user.email,
-          website: user.website,
-          password: user.password,
-          businessDetailId: businessDetails.id,
-          bankDetailId: bankDetails.id,
-        },
-      );
+        const businessDetail = await User.app.models.businessDetail.create(
+          {
+            companyName: user.companyName,
+            accountType: user.accountType,
+            categories: user.categories,
+            address: user.address,
+            completeAddress: user.completeAddress,
+            gstNo: user.gstNo,
+            tanNo: user.tanNo,
+            gstDocumentUrl: user.gstDocumentUrl,
+            panNo: user.panNo,
+            panCardUrl: user.panCardUrl,
+            userId: newUser.id,
+          },
+        );
+
+        const bankDetail = await User.app.models.bankDetail.create(
+          {
+            accountName: user.accountName,
+            accountNumber: user.accountNumber,
+            iFSCCode: user.iFSCCode,
+            bankName: user.bankName,
+            bankBranch: user.bankBranch,
+            bankState: user.bankState,
+            bankCity: user.bankCity,
+            addressProofUrl: user.addressProofUrl,
+            cancelledChequeUrl: user.cancelledChequeUrl,
+            userId: newUser.id,
+          },
+        );
+
+        return {
+          id: newUser.id,
+          imageUrl: newUser.imageUrl,
+          firstName: newUser.firstName,
+          lastName: newUser.lastName,
+          phoneNumber: newUser.phoneNumber,
+          companyName: businessDetail.companyName,
+          accountType: businessDetail.accountType,
+          categories: businessDetail.categories,
+          businessDetailsId: businessDetail.id,
+          bankDetailsId: bankDetail.id,
+        };
+      } else {
+        throw Error('Phone number has to be unique');
+      }
     } else {
-      throw Error('firstName, phoneNumber and password are required to create a user.');
+      throw Error('First name, Phone number and password are required to create a user.');
     }
   };
+  /**-------------------------------- End ------------------------------------*/
 
-  /**Get fullUserInformation method*/
-  User.remoteMethod('fullUserInformation', {
+  /**------------------------- Check phone number ----------------------------*/
+  User.remoteMethod('checkPhoneNumber', {
     accepts: {
-      arg: 'userId',
+      arg: 'phoneNumber',
       type: 'string',
       required: true,
     },
     returns: {
-      arg: 'user',
-      type: 'object',
+      arg: 'exists',
+      type: 'boolean',
       root: true,
     },
-    http: {path: '/fullUserInformation/:userId', verb: 'post'},
-    description: 'Fetches full user information',
+    http: {path: '/checkPhoneNumber/:phoneNumber', verb: 'post'},
+    description: 'Checks if phone number is registered',
   });
 
-  User.fullUserInformation = async function(userId) {
-    const user = await User.findOne({where: {id: {eq: userId}}});
-    if (user == undefined) throw Error('user not found');
-    else {
-      const businessDetails = await User.app.models.businessDetail.findOne({where: {id: {eq: user.businessDetailId}}});
-      const bankDetails = await User.app.models.bankDetail.findOne({where: {id: {eq: user.bankDetailId}}});
-      return {
-        imageUrl: user.imageUrl,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        phoneNumber: user.phoneNumber,
-        country: user.country,
-        city: user.city,
-        email: user.email,
-        website: user.website,
-        password: user.password,
-        companyName: businessDetails.companyName,
-        accountType: businessDetails.accountType,
-        categories: businessDetails.categories,
-        address: businessDetails.address,
-        completeAddress: businessDetails.completeAddress,
-        gstNo: businessDetails.gstNo,
-        tanNo: businessDetails.tanNo,
-        gstDocumentUrl: businessDetails.gstDocumentUrl,
-        panNo: businessDetails.panNo,
-        panCardUrl: businessDetails.panCardUrl,
-        accountName: bankDetails.accountName,
-        accountNumber: bankDetails.accountNumber,
-        iFSCCode: bankDetails.iFSCCode,
-        bankName: bankDetails.bankName,
-        bankBranch: bankDetails.bankBranch,
-        bankState: bankDetails.bankState,
-        bankCity: bankDetails.bankCity,
-        addressProofUrl: bankDetails.addressProofUrl,
-        cancelledChequeUrl: bankDetails.cancelledChequeUrl,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-      };
-    }
+  User.checkPhoneNumber = async function(phoneNumber) {
+    const searchedUser = await User.findOne({where: {phoneNumber: phoneNumber}});
+    return searchedUser != undefined;
   };
 };
 
